98c836c329b6b363820783643087c8a84bbe3f10
Aithusa <32020141+Aithosa@users.noreply.github.com>
2019-12-08 16:32:38 +0800

    add all

---
+++ infection_range/results/plot_fig.m
@@ -0,0 +1,8 @@
+plot(i_t(1,:)); hold on;
+plot(i_t(2,:)); hold on;
+plot(i_t(3,:)); hold on;
+plot(i_t(4,:)); hold on;
+plot(i_t(5,:)); hold on;
+plot(i_t(6,:)); hold on;
+legend('a=0.1', 'a=0.2', 'a=0.4', 'a=0.6', 'a=0.8', 'a=1')
+grid on;
